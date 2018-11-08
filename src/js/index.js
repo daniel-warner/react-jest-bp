5979b1ba102680798a7c5ca608837a5bfa98f13e
daniel-warner <daniel.reid.warner@gmail.com>
2018-11-08 06:02:36 -0700

    Initial push

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import '../scss/main.scss';
+
+import App from './components/app';
+
+
+ReactDOM.render(
+  <App/>
+  , document.getElementById("app")
+);
